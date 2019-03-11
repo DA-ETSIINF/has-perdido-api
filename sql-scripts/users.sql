c83b16cc58ee15a409ad2558263bda2f44d7d54a
onmax <maximogarciamtnez@gmail.com>
2019-03-11 22:57:58 +0100

    added tables scripts

---
+++ sql-scripts/users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
